b293660a9aacc04ba974a28e8951584b56eb549b
zackarno <zackarno@gmail.com>
2022-10-03 09:57:31 +0300

    func to load hh variables indicated by team as critical

---
+++ R/load_core_hh_indicators.R
@@ -0,0 +1,12 @@
+load_core_hh_indicators <-  function(country_code="irq"){
+  if (country_code=="irq"){
+    list(
+      `Food Consumption Score (numeric)`= "fcs",
+      `Food Consumption Score (categorical)`= "fcs_category",
+      #### continue: 
+    )
+    
+  }
+}
+
+
