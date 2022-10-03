842e95a850a1d92cb58f8129d28d134eb0f29fea
jahan-ishrat <ijnimpi@gmail.com>
2022-10-03 12:21:01 +0300

    adding initail commit

--- R/load_core_hh_indicators.R
+++ R/load_core_hh_indicators.R
@@ -9,7 +9,12 @@ load_core_hh_indicators <-  function(country_code="irq"){
     list(
       `Food Consumption Score (numeric)`= "fcs",
       `Food Consumption Score (categorical)`= "fcs_category",
-      #### continue: 
+      `Household Hunger Scale (categorical)` = "household_hunger_scale",
+      `main source of food (categorical)` = "food_source",
+      `HHH unemployed (categorical)` = "unemployed_seek_work",
+      `HH debt (numeric)` = "how_much_debt",
+      `debt per member (categorical)` = "g37"
+      
     )
     
   }
