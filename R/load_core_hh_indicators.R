88c59cd0ce9a092970eb59332f808c77d428e89d
jahan-ishrat <ijnimpi@gmail.com>
2022-10-04 16:05:13 +0300

    removed primary_livelihood and added income, med exp and health issue chronic

--- R/load_core_hh_indicators.R
+++ R/load_core_hh_indicators.R
@@ -13,9 +13,11 @@ load_core_hh_indicators <-  function(country_code="irq"){
       `main source of food (categorical)` = "food_source",
       `HHH unemployed (categorical)` = "head_seek_work",
       `HH debt (numeric)` = "how_much_debt",
-      `debt per member > 90k (categorical)` = "g37"
-      `primary_livelihood (categorical)` = "primary_livelihood"
-      
+      `debt per member > 90k (categorical)` = "g37",
+      `HH income (numeric)`= "inc_employment_pension",
+      `Medical expense` = "medical_exp",
+      `Health issues chronic` = "health_issue_chronic"
+    
       
     )
     
