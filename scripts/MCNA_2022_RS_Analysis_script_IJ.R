38de319ea039599ccba21684e4cc3aa976bbddb0
jahan-ishrat <ijnimpi@gmail.com>
2022-10-05 11:35:09 +0300

    Updated T-test

--- scripts/MCNA_2022_RS_Analysis_script_IJ.R
+++ scripts/MCNA_2022_RS_Analysis_script_IJ.R
@@ -12,48 +12,41 @@ options(scipen = 999)
 
 ## Calling dataset
 
-data_set <- fetch_msna(country_code = "irq")
+data_set <- read.csv("data/irq_clean_data_with_rs_indicators.csv")
+
+## Filtering out unwanted rows
 
 data_set <- data_set %>% filter(strata_and_pop_group != "Returnee HH_al_rutba")
 
-## converting distances by log
-
-data_set <- data_set$Distance %>% log()
-dataset <- data_set$rs_avg_dist_perm_water_pix_20172020 %>% log()
-
-### Target MCNA Variables -- Food consumption score, Hunger scale score, debt value, main source of food, HH unemployed
-
-## Pearson correlation test -- continuous + continuous
-
-
-# variable details --------------------------------------------------------
-
-# variable 1: fcs 
-      # what is the distributions?
-
+## Variable prep
 data_set$how_much_debt_log <- data_set$how_much_debt %>% log()
 
-# variable 2; NDVI
-     # what is the distribution
 data_set$Distance_log <- data_set$Distance %>% log10() 
 
 
-# test between variable 1 and variable 2 
-
-# what are the prerequisites for pearson correlation check?
+# Tests
+# MCNA variables to check -- Food consumption score -- fcs, Hunger scale, Unemployed HHH, Debt per person, Debt HH, main source of food
 
 
 fcs_correlation_check <- weighted_pearson_test(data = data_set,dep_var = "fcs", 
-                                              strata = "strata_and_pop_group",survey_weights = "survey_weight",
-                                              ind_var = c("rs_NDVI_pct_median_May2022","rs_VCI_May2022","Distance_log"))
+                                               strata = "strata_and_pop_group",survey_weights = "survey_weight",
+                                               ind_var = c("rs_NDVI_pct_median_Mar2022","rs_VCI_Mar2022","Distance_log", "rs_NDVI_z_score_Mar2022", "rs_NDVI_Mar2022" ))
 
 
 debt_correlation_check <- weighted_pearson_test(data = data_set,dep_var = "how_much_debt_log", 
-                                               strata = "strata_and_pop_group",survey_weights = "survey_weight",
-                                               ind_var = c("rs_NDVI_pct_median_May2022","rs_VCI_May2022","Distance_log"))
+                                                strata = "strata_and_pop_group",survey_weights = "survey_weight",
+                                                ind_var = c("rs_NDVI_pct_median_May2022","rs_VCI_May2022","Distance_log"))
 
 
-all_correlation_check <- fcs_correlation_check %>% bind_rows(debt_correlation_check) 
+unemplyed_relation_check <- illuminate::weighted_t_test(data = data_set, 
+                                                        binary_variable = "head_seek_work",
+                                                        non_binary_variable =   c("rs_NDVI_pct_median_May2022","rs_VCI_May2022","Distance_log"),
+                                                        strata = "strata_and_pop_group",
+                                                        survey_weights = "survey_weight")
+
+
+
+all_correlation_check <- fcs_correlation_check %>% bind_rows(debt_correlation_check,unemplyed_relation_check) 
 
 
 write_list <- list(
